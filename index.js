7ea530ace8ef8f0eb630a449f9797250b226b0fb
Avisheikbarua1158 <avisheik.barua@northsouth.edu>
2022-05-26 21:13:14 +0600

    item api done

--- index.js
+++ index.js
@@ -24,27 +24,7 @@ async function run() {
             res.send(items);
         });
 
-        app.get('/item/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const item = await itemCollection.findOne(query);
-            res.send(item);
-        });
-        app.post('/order', async (req, res) => {
-            const order = req.body;
-            if (order.quantityItem < order.minQuantity || order.quantityItem > order.availableQuantity) {
-                const query = { quantityItem: order.quantityItem, minQuantity: order.minQuantity, availableQuantity: order.availableQuantity }
-                const exists = await orderCollection.findOne(query);
-                return res.send({ success: false, order: exists })
 
-
-
-            }
-            const result = await orderCollection.insertOne(order);
-            return res.send({ success: true, result });
-
-
-        });
     }
     finally {
 
